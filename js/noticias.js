187f20c4246d5ace61196f669e0a9d52f5d25095
Carol Fantinate <carolfantinate@outlook.com>
2026-06-25 20:16:29 +0000

    artigos na pagina de noticias

--- js/noticias.js
+++ js/noticias.js
@@ -21,18 +21,46 @@ materias
         gridNoticias.appendChild(card);
     });
 
-document.getElementById("img-not-principal").src =
-    noticiaPrincipal.imagemPrincipal;
+document.getElementById("img-not-principal").src = noticiaPrincipal.imagemPrincipal;
 
-document.getElementById("titulo-principal").textContent =
-    noticiaPrincipal.titulo;
+document.getElementById("titulo-principal").textContent = noticiaPrincipal.titulo;
 
-document.getElementById("subtitulo-principal").textContent =
-    noticiaPrincipal.subtitulo;
+document.getElementById("subtitulo-principal").textContent = noticiaPrincipal.subtitulo;
 
-document.getElementById("descricao-principal").textContent =
-    noticiaPrincipal.descricao;
+document.getElementById("desc-principal").textContent = noticiaPrincipal.descricao;
 
 function acessarMateria() {
     window.location.href = "materia.html?id=1"
+}
+
+//---------artigos----------
+const grupoColunas =
+    document.getElementById("grupo-colunas");
+
+if (grupoColunas) {
+
+    artigos.slice(0, 2).forEach(artigo => {
+
+        grupoColunas.innerHTML += `
+
+            <article class="card-coluna-sulista">
+
+                <h3>
+                    ${artigo.titulo}
+                </h3>
+
+                <p>
+                    ${artigo.descricao}
+                </p>
+
+                <a
+                    href="artigo.html?id=${artigo.id}"
+                    class="card-coluna-sulista-btn">
+
+                    Ler Mais
+                </a>
+
+            </article>
+        `;
+    });
 }
