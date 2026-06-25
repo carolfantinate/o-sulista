37f5c2d9a8babf86a6a1d3f0df308cf4fdd3aa17
Carol Fantinate <carolfantinate@outlook.com>
2026-06-25 00:46:25 +0000

    conectando materias na pagina de noticias

--- js/noticias.js
+++ js/noticias.js
@@ -1,19 +1,23 @@
 const noticias = [
     {
-        titulo: "Nova tecnologia promete revolucionar a IA",
+        id: 2,
+        titulo: "Prefeitura barra arranha-céu em área turística de Balneário Camboriú",
         autor: "João Silva",
         imagem: "https://www.cma.com.br/wp-content/uploads/2023/01/placeholder-87-1.png"
     },
     {
+        id: 2,
         titulo: "Brasil vence campeonato internacional",
         autor: "Maria Souza",
         imagem: "https://www.cma.com.br/wp-content/uploads/2023/01/placeholder-87-1.png"
     },
     {
+        id: 2,
         titulo: "Descoberta científica surpreende pesquisadores",
         autor: "Pedro Santos",
         imagem: "https://www.cma.com.br/wp-content/uploads/2023/01/placeholder-87-1.png"
-    }, {
+    }, 
+    {
         titulo: "Nova tecnologia promete revolucionar a IA",
         autor: "João Silva",
         imagem: "https://www.cma.com.br/wp-content/uploads/2023/01/placeholder-87-1.png"
@@ -37,9 +41,11 @@ noticias.forEach(noticia => {
     card.classList.add("card-noticia");
 
     card.innerHTML = `
+    <a href="materia.html?id=${noticia.id}">
         <img src="${noticia.imagem}" alt="${noticia.titulo}">
         <h3>${noticia.titulo}</h3>
         <p>${noticia.autor}</p>
+        </a>
     `;
 
     gridNoticias.appendChild(card);
